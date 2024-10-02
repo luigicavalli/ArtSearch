4010369226b80308b65dbf83b9f4a677e34bbf50
Luigi Cavalli <luigi_cavalli@outlook.com>
2024-10-03 00:21:32 +0200

    fixed login and sign up forms background

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -14,38 +14,40 @@ export function Login() {
   } = useHandleLogin();
 
   return (
-    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-slate-300">
-      <h3 className="relative top-[-60px] font-bold text-3xl">Accedi</h3>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-        <Textbox
-          type="email"
-          name="email"
-          value={formData.email}
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        <Textbox
-          type={showPassword ? 'text' : 'password'}
-          name="password"
-          value={formData.password}
-          placeholder="Password"
-          onChange={handleChange}
-        />
-        <div className="flex justify-center gap-2">
-          <Checkbox checked={showPassword} onChange={toggleShowPassword} />
-          <span>Mostra password</span>
-        </div>
-        <Button type="submit" text="Login" />
-        <div className="flex gap-1 justify-center items-center">
-          <span>Non sei registrato?</span>
-          <Link
-            className="underline hover:text-sky-900 active:text-gray-900"
-            to={'/register'}
-          >
-            Registrati
-          </Link>
-        </div>
-      </form>
+    <div className="flex flex-col justify-center items-center w-full min-h-screen custom-bg">
+      <div className="flex flex-col p-8 justify-center items-center border-2 rounded-lg border-sky-900 bg-slate-100">
+        <h3 className="mb-[60px] font-bold text-3xl">Accedi</h3>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+          <Textbox
+            type="email"
+            name="email"
+            value={formData.email}
+            placeholder="Email"
+            onChange={handleChange}
+          />
+          <Textbox
+            type={showPassword ? 'text' : 'password'}
+            name="password"
+            value={formData.password}
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <div className="flex justify-center gap-2">
+            <Checkbox checked={showPassword} onChange={toggleShowPassword} />
+            <span>Mostra password</span>
+          </div>
+          <Button type="submit" text="Login" />
+          <div className="flex gap-1 justify-center items-center">
+            <span>Non sei registrato?</span>
+            <Link
+              className="underline hover:text-sky-900 active:text-gray-900"
+              to={'/register'}
+            >
+              Registrati
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -14,52 +14,54 @@ export function Register() {
   } = useHandleSignUp();
 
   return (
-    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-slate-300">
-      <h3 className="relative top-[-60px] font-bold text-3xl">Registrati</h3>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
-        <Textbox
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          placeholder={'Nome'}
-        />
-        <Textbox
-          type="text"
-          name="surname"
-          value={formData.surname}
-          onChange={handleChange}
-          placeholder={'Cognome'}
-        />
-        <Textbox
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder={'Email'}
-        />
-        <Textbox
-          type={showPassword ? 'text' : 'password'}
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder={'Password'}
-        />
-        <div className="flex justify-center gap-2">
-          <Checkbox checked={showPassword} onChange={toggleShowPassword} />
-          <span>Mostra password</span>
-        </div>
-        <Button type="submit" text="Invia" />
-        <div className="flex gap-1 justify-center items-center">
-          <span>Gi&agrave; registrato?</span>
-          <Link
-            className="underline hover:text-sky-900 active:text-gray-900"
-            to={'/login'}
-          >
-            Login
-          </Link>
-        </div>
-      </form>
+    <div className="flex flex-col justify-center items-center w-full min-h-screen custom-bg">
+      <div className="flex flex-col p-8 justify-center items-center border-2 rounded-lg border-sky-900 bg-slate-100">
+        <h3 className="mb-[60px] font-bold text-3xl">Registrati</h3>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+          <Textbox
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder={'Nome'}
+          />
+          <Textbox
+            type="text"
+            name="surname"
+            value={formData.surname}
+            onChange={handleChange}
+            placeholder={'Cognome'}
+          />
+          <Textbox
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder={'Email'}
+          />
+          <Textbox
+            type={showPassword ? 'text' : 'password'}
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            placeholder={'Password'}
+          />
+          <div className="flex justify-center gap-2">
+            <Checkbox checked={showPassword} onChange={toggleShowPassword} />
+            <span>Mostra password</span>
+          </div>
+          <Button type="submit" text="Invia" />
+          <div className="flex gap-1 justify-center items-center">
+            <span>Gi&agrave; registrato?</span>
+            <Link
+              className="underline hover:text-sky-900 active:text-gray-900"
+              to={'/login'}
+            >
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
